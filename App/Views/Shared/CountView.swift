0265f5b3529b2a709c8423ae97485d5db7eee762
Levitating Pineapple <noreply@levitatingpineapple.com>
2024-01-13 23:33:52 +0100

    Refactor CountView.Model

--- App/Views/Shared/CountView.swift
+++ App/Views/Shared/CountView.swift
@@ -2,38 +2,40 @@ import SwiftUI
 import GRDBQuery
 import Combine
 
-@Observable class Test {
-	var count: Int = .zero
-	private var bag = Set<AnyCancellable>()
-	
-	init(filter: Filter) {
-		Item.RequestCount(filter: filter)
-			.publisher(in: StoreKey.defaultValue)
-			.receive(on: DispatchQueue.main)
-			.sink(
-				receiveCompletion: { _ in },
-				receiveValue: { [weak self] value in self?.count = value }
-			)
-			.store(in: &bag)
-	}
-}
-
 struct CountView: View {
-	@State private var test: Test
+	@State private var model: Model
 	
 	init(filter: Filter) {
-		self.test = Test(filter: filter)
+		self.model = Model(filter: filter)
 	}
 	
 	var body: some View {
-		if test.count > .zero {
-			Text(String(test.count))
+		if model.count > .zero {
+			Text(String(model.count))
 				.contentTransition(.numericText())
-				.animation(.default, value: test.count)
+				.animation(.default, value: model.count)
 				.lineLimit(1)
 				.foregroundStyle(Color.white)
 				.font(.caption).fontWeight(.heavy)
 				.padding(.vertical, 4).padding(.horizontal, 8)
+		}
+	}
+}
+
+extension CountView {
+	@Observable class Model {
+		var count: Int = .zero
+		private var bag = Set<AnyCancellable>()
+		
+		init(filter: Filter) {
+			Item.RequestCount(filter: filter)
+				.publisher(in: StoreKey.defaultValue)
+				.receive(on: DispatchQueue.main)
+				.sink(
+					receiveCompletion: { _ in },
+					receiveValue: { [weak self] value in self?.count = value }
+				)
+				.store(in: &bag)
 		}
 	}
 }
